afaaaff3605a2297773a5dd99f705438745a4a44
Nick Rodriguez <nickrod518@gmail.com>
2019-05-06 17:30:12 -0400

    Add optional params for cmd and last_only

--- ssh_helper.py
+++ ssh_helper.py
@@ -1,10 +1,11 @@
 from fabric import Connection
 
 
-def login_and_execute(host, usr, pwd, cmd):
+def login_and_execute(host, usr, pwd, cmd="hostname"):
     print("{} = {}".format(usr, pwd))
 
     c = Connection(host=host, user=usr, connect_kwargs={"password": pwd})
+    # hide stderr and stdout
     result = c.run(cmd, hide=True)
     c.close()
 
@@ -12,13 +13,18 @@ def login_and_execute(host, usr, pwd, cmd):
     return result.stdout.splitlines()[1]
 
 
-def test_solutions(host, solutions, last_only):
+def test_solutions(host, solutions, last_only=False):
+    print("\n\nhost = {}\n".format(host))
+
     if last_only == True:
         last_sol = list(solutions.keys())[-2]
         next_sol = list(solutions.keys())[-1]
+
         pwd = login_and_execute(
             host, last_sol, solutions[last_sol][1], solutions[last_sol][0])
-        login_and_execute(host, next_sol, pwd, "hostname")
+
+        login_and_execute(host, next_sol, pwd)
+
     else:
         next_pwd = solutions.get((list(solutions.keys()))[0])[1]
 
@@ -29,4 +35,4 @@ def test_solutions(host, solutions, last_only):
             if cmd is not None:
                 next_pwd = login_and_execute(host, usr, pwd, cmd)
             else:
-                login_and_execute(host, usr, pwd, "hostname")
+                login_and_execute(host, usr, pwd)
