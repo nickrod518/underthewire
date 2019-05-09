503d1e4fd36c42b74d09059a62259db45f65f2e4
Nick Rodriguez <nickrod518@gmail.com>
2019-05-08 23:29:28 -0400

    Add solutions through 6 and update to use multiline strings.

--- cyborg.py
+++ cyborg.py
@@ -3,8 +3,26 @@ from ssh_helper import test_solutions
 # username: (command, password)
 solutions = {
     "cyborg1": ("(Get-ADUser -Filter {Surname -eq 'Rogers' -and GivenName -eq 'Chris'} -Properties State).State", "cyborg1"),
-    "cyborg2": (None, "kansas"),
-    "cyborg3": (None, None)
+    "cyborg2": (
+        """
+            $a = (Get-DnsServerResourceRecord -RRType A -ZoneName underthewire.tech -Name CYBORG718W100N).RecordData.IPv4Address.IPAddressToString
+            $b = (gci ~\Desktop).Name
+            $a + $b
+        """,
+        "kansas"),
+    "cyborg3": ("(Get-ADGroupMember Cyborg -Recursive).Count.ToString() + (gci ~\Desktop).Name", "172.31.45.167_ipv4"),
+    "cyborg4": ("(Get-Command | where Version -eq '8.9.8.9').Source + (gci ~\Desktop).Name", "88_objects"),
+    "cyborg5": (
+        """
+            $a = (Get-ADUser -Filter {LogonHours -like '*' -and Enabled -ne $true} -Properties LogonHours).Surname
+            $b = (gci ~\Desktop).Name
+            $a + $b
+        """,
+        "bacon_eggs"
+    ),
+    "cyborg6": (
+        None, "rowray_timer"
+    )
 }
 
-test_solutions("cyborg.underthewire.tech", solutions)
+test_solutions("cyborg.underthewire.tech", solutions, False)
